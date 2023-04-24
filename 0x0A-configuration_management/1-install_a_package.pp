5ebe09e4d42f3f31751fecc53f3b16cc105d82e5
Robin60 <owinorobin05@gmail.com>
2023-04-24 19:40:26 +0000

    feat: Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # install flask -v 2.1.0 via pip3
 
 exec { 'flask':
-  command => '/usr/bin/pip3 install Flask -v 2.1.0',
+     command => '/usr/bin/pip3 install Flask==2.1.0',
+       creates => '/usr/local/lib/python3.8/dist-packages/flask',
 }
